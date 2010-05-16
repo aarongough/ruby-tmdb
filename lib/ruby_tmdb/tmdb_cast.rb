b9e2802267715c420bd91d882e6a4298c6555ca8
Aaron Gough <aaron@aarongough.com>
2010-05-16 17:00:52 -0400

    Updated tests and added code for TmdbCast

---
+++ lib/ruby_tmdb/tmdb_cast.rb
@@ -0,0 +1,67 @@
+class TmdbCast
+
+  def self.find(options)
+    raise ArgumentError, "At least one of: id, name, should be supplied" if(options[:id].nil? && options[:name].nil?)
+    
+    results = []
+    unless(options[:id].nil? || options[:id].to_s.empty?)
+      results << Tmdb.api_call('Person.getInfo', options[:id])
+    end
+    unless(options[:name].nil? || options[:name].to_s.empty?)
+      results << Tmdb.api_call('Person.search', options[:name])
+    end
+    
+    results.flatten!
+    
+    unless(options[:limit].nil?)
+      raise ArgumentError, ":limit must be an integer greater than 0" unless(options[:limit].is_a?(Fixnum) && options[:limit] > 0)
+      results = results.slice(0, options[:limit])
+    end
+  
+    results.map!{|c| TmdbCast.new(c) }
+  
+    if(results.length == 1)
+      return results[0]
+    else
+      return results
+    end
+  end
+  
+  def initialize(raw_data)
+    @raw_data = raw_data
+    @raw_data = Tmdb.api_call('Person.getInfo', @raw_data["id"]).first
+    @raw_data["profiles"] = @raw_data["profile"]
+    @raw_data.delete("profile")
+    @raw_data.each_pair do |key, value|
+      instance_eval <<-EOD
+        def #{key}
+          @raw_data["#{key}"]
+        end
+      EOD
+      if(value.is_a?(Array))
+        value.each_index do |x|
+          if(value[x].is_a?(Hash) && value[x].length == 1)
+            if(value[x].keys[0] == "image")
+              value[x][value[x].keys[0]].instance_eval <<-EOD
+                def self.data
+                  Tmdb.get_url(self["url"]).body
+                end
+              EOD
+            end
+            value[x] = value[x][value[x].keys[0]]
+          end
+          if(value[x].is_a?(Hash))
+            value[x].each_pair do |key2, value2|
+              value[x].instance_eval <<-EOD
+                def self.#{key2}
+                  self["#{key2}"]
+                end
+              EOD
+            end
+          end
+        end
+      end
+    end
+  end
+  
+end
--- test/unit/tmdb_cast_test.rb
+++ test/unit/tmdb_cast_test.rb
@@ -5,61 +5,84 @@ class TmdbCastTest < Test::Unit::TestCase
   def setup
     register_api_url_stubs
   end
+  
+  test "find by id should return full cast data" do
+    cast = TmdbCast.find(:id => 287)
+    assert_cast_methodized(cast, 287)
+  end
+  
+  test "find by name should return full cast data" do
+    cast = TmdbCast.find(:name => "Brad Pitt").first
+    assert_cast_methodized(cast, 287)
+  end
 
-#  test "find by id and name should all return full cast data" do
-#    @test_cast_members = []
-#    @test_cast_members << TmdbCast.find(:id => 287)
-#    @test_cast_members << TmdbCast.find(:name => "Brad Pitt").first
-#    @cast_data = YAML::load(Tmdb.get_url("http://api.themoviedb.org/2.1/Person.getInfo/en/yaml/#{Tmdb.api_key}/287").body)[0]
-#    @test_cast_members.each do |actor|
-#      assert_equal @cast_data["popularity"], actor.popularity
-#      assert_equal @cast_data["name"], actor.name
-#      assert_equal @cast_data["known_as"], actor.known_as
-#      assert_equal @cast_data["id"], actor.id
-#      assert_equal @cast_data["biography"], actor.biography
-#      assert_equal @cast_data["known_movies"], actor.known_movies
-#      assert_equal @cast_data["birthday"], actor.birthday
-#      assert_equal @cast_data["birthplace"], actor.birthplace
-#      assert_equal @cast_data["url"], actor.url
-#      @cast_data["filmography"].each_index do |x|
-#        assert_equal @cast_data["filmography"][x]["name"], actor.filmography[x].name
-#        assert_equal @cast_data["filmography"][x]["id"], actor.filmography[x].id
-#        assert_equal @cast_data["filmography"][x]["job"], actor.filmography[x].job
-#        assert_equal @cast_data["filmography"][x]["department"], actor.filmography[x].department
-#        assert_equal @cast_data["filmography"][x]["character"], actor.filmography[x].character
-#        assert_equal @cast_data["filmography"][x]["url"], actor.filmography[x].url
-#      end
-#      @cast_data["filmography"].each_index do |x|
-#        assert_equal TmdbMovie.find(:id => @cast_data["filmography"][x]["id"]), actor.movies[x]
-#      end
-#      @cast_data["profile"].each_index do |x|
-#        assert_equal @cast_data["profile"][x]["image"]["type"], actor.profiles[x].type
-#        assert_equal @cast_data["profile"][x]["image"]["size"], actor.profiles[x].size
-#        assert_equal @cast_data["profile"][x]["image"]["height"], actor.profiles[x].height
-#        assert_equal @cast_data["profile"][x]["image"]["width"], actor.profiles[x].width
-#        assert_equal @cast_data["profile"][x]["image"]["url"], actor.profiles[x].url
-#        assert_equal @cast_data["profile"][x]["image"]["id"], actor.profiles[x].id
-#        assert_equal Tmdb.get_url(@cast_data["profile"][x]["image"]["url"]), actor.profiles[x].data
-#      end
-#    end
-#  end
-#  
-#  test "should raise exception if no arguments supplied to find" do
-#    assert_raise ArgumentError do
-#      TmdbCast.find()
-#    end
-#  end
-#
-#  test "find by id should return a single cast member" do
-#    assert_kind_of TmdbCast.find(:id => 287), "TmdbCast"
-#  end
-#  
-#  test "find by name should return an array of cast members" do
-#    cast_members = TmdbCast.find(:name => "vince")
-#    assert_kind_of cast_members, "Array"
-#    cast_members.each do |actor|
-#      assert_kind_of actor, "TmdbCast"
-#    end
-#  end
+  test "should raise exception if no arguments supplied to find" do
+    assert_raise ArgumentError do
+      TmdbCast.find()
+    end
+  end
+
+  test "find by id should return a single cast member" do
+    assert_kind_of TmdbCast, TmdbCast.find(:id => 287)
+  end
+  
+  test "find by name should return an array of cast members" do
+    cast_members = TmdbCast.find(:name => "vince")
+    assert_kind_of Array, cast_members
+    cast_members.each do |actor|
+      assert_kind_of TmdbCast, actor
+    end
+  end
+  
+  test "should raise error if limit is smaller than 1" do
+    [0, -1, -100].each do |limit|
+      assert_raise ArgumentError do
+        TmdbCast.find(:name => "vince", :limit => limit)
+      end
+    end
+  end
+  
+  test "should raise error if limit is not an integer" do
+    [1.001, "1.2", "hello", [1,2,3], {:test => "1"}].each do |limit|
+      assert_raise ArgumentError do
+        TmdbCast.find(:name => "vince", :limit => limit)
+      end
+    end
+  end
+  
+  private
+  
+    def assert_cast_methodized(actor, cast_id)
+      @cast_data = Tmdb.api_call('Person.getInfo', cast_id)[0]
+      assert_equal @cast_data["popularity"], actor.popularity
+      assert_equal @cast_data["name"], actor.name
+      assert_equal @cast_data["known_as"], actor.known_as
+      assert_equal @cast_data["id"], actor.id
+      assert_equal @cast_data["biography"], actor.biography
+      assert_equal @cast_data["known_movies"], actor.known_movies
+      assert_equal @cast_data["birthday"], actor.birthday
+      assert_equal @cast_data["birthplace"], actor.birthplace
+      assert_equal @cast_data["url"], actor.url
+      @cast_data["filmography"].each_index do |x|
+        assert_equal @cast_data["filmography"][x]["name"], actor.filmography[x].name
+        assert_equal @cast_data["filmography"][x]["id"], actor.filmography[x].id
+        assert_equal @cast_data["filmography"][x]["job"], actor.filmography[x].job
+        assert_equal @cast_data["filmography"][x]["department"], actor.filmography[x].department
+        assert_equal @cast_data["filmography"][x]["character"], actor.filmography[x].character
+        assert_equal @cast_data["filmography"][x]["url"], actor.filmography[x].url
+      end
+      #@cast_data["filmography"].each_index do |x|
+      #  assert_equal TmdbMovie.find(:id => @cast_data["filmography"][x]["id"]), actor.movies[x]
+      #end
+      @cast_data["profile"].each_index do |x|
+        assert_equal @cast_data["profile"][x]["image"]["type"], actor.profiles[x].type
+        assert_equal @cast_data["profile"][x]["image"]["size"], actor.profiles[x].size
+        assert_equal @cast_data["profile"][x]["image"]["height"], actor.profiles[x].height
+        assert_equal @cast_data["profile"][x]["image"]["width"], actor.profiles[x].width
+        assert_equal @cast_data["profile"][x]["image"]["url"], actor.profiles[x].url
+        assert_equal @cast_data["profile"][x]["image"]["id"], actor.profiles[x].id
+        assert_equal Tmdb.get_url(@cast_data["profile"][x]["image"]["url"]).body, actor.profiles[x].data
+      end
+    end
 
 end
