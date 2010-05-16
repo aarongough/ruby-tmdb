ef8f111c7ea3209b93a4ba7e84da06dac9e34815
Aaron Gough <aaron@aarongough.com>
2010-05-16 17:04:38 -0400

    Deleted unused files from directory structure. Removed extra whitespace from url_mocks.rb

--- test/setup/url_mocks.rb
+++ test/setup/url_mocks.rb
@@ -32,7 +32,6 @@ def register_api_url_stubs
       stub_request(:get, Regexp.new('http://i[0-9].themoviedb.org/[backdrops|posters|profiles].*')).to_return(file)
     end
     
-    
     stub_request(:get, 'http://thisisaurlthatdoesntexist.co.nz').to_return(:body => "", :status => 404, :headers => {'content-length' => 0})
   end
 end
