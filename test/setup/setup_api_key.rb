a097b2ef5a8d290c428c8530c559bf49d734104d
Irio Irineu Musskopf Junior <irio@startocean.com>
2012-05-19 13:28:21 -0300

    Prettier output telling to create tmdb_api_key.txt

--- test/setup/setup_api_key.rb
+++ test/setup/setup_api_key.rb
@@ -1,3 +1,10 @@
-File.open(File.join(File.dirname(__FILE__), 'tmdb_api_key.txt')) do |file|
-  Tmdb.api_key = file.read
+begin
+  file_path = File.absolute_path('tmdb_api_key.txt', File.dirname(__FILE__))
+
+  File.open(file_path) do |file|
+    Tmdb.api_key = file.read
+  end
+rescue Errno::ENOENT => e
+  $stderr.puts "You need place your api key in #{file_path}"
+  exit
 end
